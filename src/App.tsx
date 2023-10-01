86f40245191932be7024a2e7a6906acf609d9fc4
Bruno Alves <brunoalves@Mac-mini-de-Bruno.local>
2023-10-01 16:27:42 -0300

    feat(state): add state manager via provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { FileUpload } from "./components/atoms/fileUpload"
+import { BatchesProvider } from "./provider"
 
 function App() {
 
   return (
-    <div className="w-screen h-screen bg-gray-800 flex justify-center items-center " >
-      <FileUpload onChange={console.log} name="file" description="JSON (MAX. 200mb)" label='Clique para upload do arquivo.' />
-    </div>
+    <BatchesProvider>
+      <div className="w-screen h-screen bg-gray-800 flex justify-center items-center " >
+        <FileUpload onChange={console.log} name="file" description="JSON (MAX. 200mb)" label='Clique para upload do arquivo.' />
+      </div>
+    </BatchesProvider>
   )
 }
 
